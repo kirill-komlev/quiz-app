6fa5dfa52c052d5cd36f050b9d49735c1b490a23
kirill-komlev <141944537+kirill-komlev@users.noreply.github.com>
2024-10-19 06:22:30 +0000

    Updates

--- assets/Button-DNoVUdzM.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-Dle6zYsw.js";function e({children:n,className:r,onClick:o}){return t.jsx(t.Fragment,{children:t.jsx("button",{className:r,onClick:o,children:n})})}export{e as B};
