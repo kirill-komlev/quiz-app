324202a19b9e86b1f941a6963eedbf6a337e4f8f
kirill-komlev <141944537+kirill-komlev@users.noreply.github.com>
2024-10-21 13:17:14 +0000

    Updates

---
+++ assets/Button-AqlC4XvV.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BQz9z_B0.js";function e({children:n,className:r,onClick:o}){return t.jsx(t.Fragment,{children:t.jsx("button",{className:r,onClick:o,children:n})})}export{e as B};
